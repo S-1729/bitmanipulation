e37d9a28c29a7453107f41008461977a7676b4c4
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2023-03-29 14:48:57 +0530

    Add files via upload

---
+++ CountLeadingZeros.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    
+    cout<<"Count of leading zeros = "<<__builtin_clz(n);
+}
---
+++ CountTrailingZeros.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    
+    cout<<"Count of trailing zeros = "<<__builtin_ctz(n);
+}
